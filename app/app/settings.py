3d8c1dcf0d369fda5eecc3b29aab66f0fc8b9412
Ashley D'Allessandro <ashley.dallessandro@gmail.com>
2021-07-14 11:57:03 -0700

    os troubleshoot

--- app/app/settings.py
+++ app/app/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environ.get('DB_HOST'),
+        'NAME': os.envrion.get('DB_NAME'),
+        'USER': os.envrion.get('DB_USER'),
+        'PASSWORD': os.envrion.get('DB_PASS'),
     }
 }
 
